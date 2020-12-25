119cca23104abbdd3ab9a47eca2850b2da6f3644
Hong Dah Ong <40519064+ezpher@users.noreply.github.com>
2020-12-25 19:09:21 +0800

    refactor to account for overlapping word sequences

--- wordsearch.py
+++ wordsearch.py
@@ -5,7 +5,7 @@ import random
 
 # initialization of variables
 width, height = 20, 20
-grid = [[random.choice(string.ascii_letters).upper() for j in range(width)] for i in range(height)]
+grid = [['_' for j in range(width)] for i in range(height)]
 words = ['HELLO', 'THERE', 'FRIEND']
 
 # functions
@@ -13,17 +13,32 @@ def print_grid(grid):
     print('\n'.join(map(lambda row: ' '.join(row), grid)))
 
 def construct_word(word, grid):
-    orientations = [[1, 0], [0, 1], [1, 1]]
-    selected_orientation = random.choice(orientations)
 
-    max_x_pos = len(grid) if selected_orientation[0] == 0 else len(grid)-len(word)
-    max_y_pos = len(grid[0]) if selected_orientation[1] == 0 else len(grid[0])-len(word)
+    is_placed = False
+    max_x_pos, max_y_pos, selected_x_pos, selected_y_pos = 0, 0, 0, 0
+    
+    while is_placed == False: 
+        orientations = [[1, 0], [0, 1], [1, 1]]
+        selected_orientation = random.choice(orientations)
 
-    selected_x_pos = random.randrange(0, max_x_pos)
-    selected_y_pos = random.randrange(0, max_y_pos)
+        max_x_pos = len(grid) if selected_orientation[0] == 0 else len(grid)-len(word)
+        max_y_pos = len(grid[0]) if selected_orientation[1] == 0 else len(grid[0])-len(word)
 
-    for i in range(0, len(word)):
-        grid[selected_y_pos + (selected_orientation[1] * i)][selected_x_pos + (selected_orientation[0] * i)] = word[i]
+        selected_x_pos = random.randrange(0, max_x_pos)
+        selected_y_pos = random.randrange(0, max_y_pos)
+
+        for i in range(0, len(word)):
+            pos_to_place = grid[selected_y_pos + (selected_orientation[1] * i)][selected_x_pos + (selected_orientation[0] * i)]
+            if pos_to_place == '_' or pos_to_place == word[i]:
+                is_placed = True
+                continue
+            else:
+                is_placed = False
+                break
+
+    if is_placed == True:
+        for i in range(0, len(word)):
+            grid[selected_y_pos + (selected_orientation[1] * i)][selected_x_pos + (selected_orientation[0] * i)] = word[i]
 
 def construct_words(words, grid):
     for i in range(0, len(words)):
@@ -33,5 +48,4 @@ def construct_words(words, grid):
 construct_words(words, grid)
 print_grid(grid)
 
-# note: currently, the wordsearch does not account for overlapping characters
 
